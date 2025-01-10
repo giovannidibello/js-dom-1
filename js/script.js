4b8c595dae60e6668d2470483bf0208da915e760
Giovanni <dibellogio@gmail.com>
2025-01-10 15:22:48 +0100

    add script bonus turn on/off lampadina

--- js/script.js
+++ js/script.js
@@ -6,5 +6,16 @@ const bottone = document.querySelector("button");
 
 // cambio l'immagine della lampadina al click sul pulsante
 bottone.addEventListener("click", function () {
-    lampadina.src = "./img/yellow_lamp.png"
+    
+    // controllo se il testo del bottone include ACCENDI
+    const isOn = bottone.innerHTML.includes("ACCENDI");
+
+    // se è vero accendo la lampadina e scrivo spegni altrimenti il contrario
+    if (isOn) {
+        bottone.innerHTML = "SPEGNI"
+        lampadina.src = "./img/yellow_lamp.png"
+    } else {
+        bottone.innerHTML = "ACCENDI"
+        lampadina.src = "./img/white_lamp.png"
+    }
 })
